1f5c6af8a656fd4a640876d8300c17d0ef3b4661
LuaFan2 <51294434+LuaFan2@users.noreply.github.com>
2020-11-12 22:06:23 +0500

    Add a lot of utils

---
+++ src/util/utils.py
@@ -0,0 +1,43 @@
+from os import listdir
+from os.path import isfile, join, splitext, split
+
+from enum import Enum
+
+IMAGE_FORMATS = ["bmp", "gif", "jpg", "jpeg", "png", "pbm", "pgm", "ppm", "xbm", "xpm"]
+VIDEO_FORMATS = ["avi", "mkv", "mp4", "flv", "mpeg", "mov", "ts", "m2ts", "wmv", "rm", "rmvb", "ogm", "webm"]
+AUDIO_FORMATS = ["mp3", "flc", "m4a", "aac", "ogg", "3gp", "amr", "ape", "mka", "opus", "wavpack", "musepack"]
+
+IMAGE_FILTER = "Images ({})".format(" ".join(["*.{}".format(e) for e in IMAGE_FORMATS]))
+VIDEO_FILTER = "Videos ({})".format(" ".join(["*.{}".format(e) for e in VIDEO_FORMATS]))
+AUDIO_FILTER = "Audios ({})".format(" ".join(["*.{}".format(e) for e in AUDIO_FORMATS]))
+
+
+class FILE_FORMAT(Enum):
+    IMAGE = 0,
+    VIDEO = 1,
+    AUDIO = 2,
+    INVALID = 3,
+
+
+def get_dir_files(path):
+    return [f for f in listdir(path) if isfile(join(path, f))]
+
+
+def get_file_ext(path):
+    return split(path)[1]
+
+
+def get_extension(filename):
+    _, extension = splitext(filename)
+    return extension[1:] if extension else ""
+
+
+def get_format(filename):
+    ext = get_extension(filename)
+    if ext in IMAGE_FORMATS:
+        return FILE_FORMAT.IMAGE
+    elif ext in VIDEO_FORMATS:
+        return FILE_FORMAT.VIDEO
+    elif ext in AUDIO_FORMATS:
+        return FILE_FORMAT.AUDIO
+    return FILE_FORMAT.INVALID
