af5958934402d2829ccdbc2c74330e8d76e2e9e5
PatriotRossii <patriotrossii2019@mail.ru>
2020-12-23 17:34:01 +0500

    Add get radio stations method in input manager

--- src/gui/input_manager.py
+++ src/gui/input_manager.py
@@ -2,7 +2,7 @@ from PyQt5.QtCore import QUrl, QIODevice, QFile, QDataStream, QVariant, QTimer
 from PyQt5.QtMultimedia import QMediaPlaylist, QMediaContent, QMediaPlayer, QAudioProbe, QAudio, QAudioDeviceInfo, \
     QAudioOutputSelectorControl, QMediaService
 
-from src.database.database_manager import RecentFilesManager
+from src.database.database_manager import RecentFilesManager, RadioStationsManager
 from src.util.playlist_item import PlaylistItemDataRole
 from src.util.utils import *
 from src.gui.ui_manager import UIManager
@@ -16,6 +16,7 @@ class InputManager:
     def __init__(self):
         if not InputManager.__instance:
             self.ui_manager = UIManager.get_instance()
+            self.radio_stations_manager = RadioStationsManager()
 
             self.player = None
             self.playlist = None
@@ -106,6 +107,10 @@ class InputManager:
         in_stream >> cur_playlist
 
         self.playlist = cur_playlist.value()
+
+    def get_radio_stations(self):
+        stations = self.radio_stations_manager.get_all_stations()
+        return stations
 
     def save_state(self):
         file = QFile("file.dat")
